0346b1470044a4155d3f9a56d81b50333d981dc7
Park hanbaek <totwjfakd@gmail.com>
2023-05-03 14:44:34 +0000

    dev login logout

--- openseats/views/auth_views.py
+++ openseats/views/auth_views.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, url_for, render_template, flash, request, session
+from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
@@ -9,9 +9,23 @@ from openseats.models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/SignIn')
+
+@bp.route('/SignIn', methods=('GET', 'POST'))
 def SignIn_page() :
-    return render_template('sign_in.html')
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(email=form.email.data).first()
+        if not user:
+            error = "존재하지 않는 이메일입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.main_page'))
+        flash(error)
+    return render_template('sign_in.html', form=form)
 
 @bp.route('/SignUp', methods=('GET', 'POST'))
 def SignUp_page() :
@@ -28,3 +42,16 @@ def SignUp_page() :
         else:
             flash('이미 존재하는 사용자입니다.')
     return render_template('sign_up.html', form=form)
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.main_page'))
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
