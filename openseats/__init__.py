f2bfc0b4cdb37202ddde234ea63f7c082f6e6925
Insu Oh <ohinsu134@gmail.com>
2023-05-05 17:25:35 +0000

    multiple images uploading

--- openseats/__init__.py
+++ openseats/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 import config
 
@@ -8,10 +9,13 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
-
 def create_app() :
     app = Flask(__name__, template_folder='templates')
     app.config.from_object(config)
+
+    # 파일 저장 경로 설정
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static', 'uploads', 'img')
 
     # ORM
     db.init_app(app)
--- openseats/models.py
+++ openseats/models.py
@@ -1,13 +1,37 @@
 from openseats import db
+from werkzeug.utils import secure_filename
+import os 
+from datetime import datetime
+
+
+# original
+# class Group(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     address = db.Column(db.String(200), nullable=False)
+#     description = db.Column(db.String(200), nullable=False)
+#     name = db.Column(db.String(200), nullable=False)
+#     money_per_hour = db.Column(db.Integer, nullable=False)
+#     create_date = db.Column(db.DateTime(), nullable=False)
+#     image_path = db.Column(db.String(200), nullable=False)
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(200), nullable=False)
-    name = db.Column(db.String(200), nullable=False)
     money_per_hour = db.Column(db.Integer, nullable=False)
-    create_date = db.Column(db.DateTime(), nullable=False)
-    image_path = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
+    
+    images = db.relationship('Image', backref='group', lazy=True)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    path = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,9 +40,3 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 # https://sqlalchemy-imageattach.readthedocs.io/en/1.0.0/guide/context.html#thumbnails
 
-# class Answer(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
-#     question = db.relationship('Question', backref=db.backref('answer_set', cascade='all, delete-orphan'))
-#     content = db.Column(db.Text(), nullable=False)
-#     create_date = db.Column(db.DateTime(), nullable=False)
