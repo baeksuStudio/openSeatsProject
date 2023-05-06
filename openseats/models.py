1b23f5bbc59114a6f0b588a983a1cea3873a5b54
Insu Oh <ohinsu134@gmail.com>
2023-05-06 15:40:25 +0000

    made a bit of group_detail page

--- openseats/models.py
+++ openseats/models.py
@@ -14,6 +14,14 @@ from datetime import datetime
 #     create_date = db.Column(db.DateTime(), nullable=False)
 #     image_path = db.Column(db.String(200), nullable=False)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    groups = db.relationship('Group', backref='group')
+    
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
@@ -25,6 +33,9 @@ class Group(db.Model):
     
     images = db.relationship('Image', backref='group', lazy=True)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
+
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -33,10 +44,5 @@ class Image(db.Model):
 
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'), nullable=False)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
 # https://sqlalchemy-imageattach.readthedocs.io/en/1.0.0/guide/context.html#thumbnails
 
